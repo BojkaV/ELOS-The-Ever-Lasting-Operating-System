12a4ac70f8488d7057d435c7db7c2d754b64ea23
Bojidar Vladimirov <bojkav42069@gmail.com>
2023-04-08 09:35:13 +0300

    Added login screen that uses /src/reg/passwd for a password, addded logout command and changed to build 4

--- src/ELOS.py
+++ src/ELOS.py
@@ -14,31 +14,50 @@ sleep(3)
 
 os.system(clear)
 
-print("ELOS 1 [Build 3] Ever Lasting Project")
-print("(C) ELP ELOS License")
+def ELOS():
+    print("ELOS 1 [Build 4] Ever Lasting Project")
+    print("(C) ELP ELOS License")
 
-while True:
-    commands = ['help', 'shutdown', 'info', 'clear']
-    com = input('> ')
-    if com.lower() not in commands:
-        print('Invalid Command')
-    if com.lower() == 'help':
-        print('Here are all available commands:')
-        print('help - lists all available commands')
-        print('shutdown - shuts down ELOS')
-        print('info - shows information about ELOS')
-        print('clear - clears the contents of the terminal')
-    if com.lower() == 'shutdown':
-        os.system(clear)
-        print('ELOS is shutting down...')
-        sleep(3)
-        os.system(clear)
-        exit()
-    if com.lower() == 'info':
-        print("ELOS 1 [Build 3] Ever Lasting Project")
-        print("(C) ELP ELOS License")
-    if com.lower() == 'clear':
-        os.system(clear)
+    while True:
+        commands = ['help', 'shutdown', 'info', 'clear', 'logout']
+        com = input('> ')
+        if com.lower() not in commands:
+            print('Invalid Command')
+        if com.lower() == 'help':
+            print('Here are all available commands:')
+            print('help - lists all available commands')
+            print('shutdown - shuts down ELOS')
+            print('info - shows information about ELOS')
+            print('clear - clears the contents of the terminal')
+            print('logout - prompts you to login')
+        if com.lower() == 'shutdown':
+            os.system(clear)
+            print('ELOS is shutting down...')
+            sleep(3)
+            os.system(clear)
+            exit()
+        if com.lower() == 'info':
+            print("ELOS 1 [Build 3] Ever Lasting Project")
+            print("(C) ELP ELOS License")
+        if com.lower() == 'clear':
+            os.system(clear)
+        if com.lower() == 'logout':
+            os.system(clear)
+            login()
 
-sleep(3)
+def login():
+    while True:
+        print('Type password to login')
+        print('Type exit to exit')
+        passwd = input('Password: ')
+        f = open('reg/password', 'r')
+        if passwd == f.read():
+            os.system(clear)
+            ELOS()
+        elif passwd == 'exit':
+            os.system(clear)
+            exit()
+        else:
+            print('Invalid Password')
 
+login()
